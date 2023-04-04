4e03ffa8b3eaf52cfc9eebf795db4c048cabb750
914-modolea-bogdan <bogdan.modolea@stud.ubbcluj.ro>
2023-04-04 21:33:07 +0300

    IP

--- frontend/src/team/AllTeams.js
+++ frontend/src/team/AllTeams.js
@@ -7,7 +7,8 @@ export default function AllTeams() {
     const toAddURL = 'lab-5x-914-Modolea-Bogdan/';
     // const toAddURL = '';
     // const serverLink = 'http://localhost:8080/';
-    const serverLink = 'http://myleaguemanagerlab05-env.eba-m6hmvmjt.eu-north-1.elasticbeanstalk.com';
+    // const serverLink = 'http://myleaguemanagerlab05-env.eba-m6hmvmjt.eu-north-1.elasticbeanstalk.com';
+    const serverLink = '13.49.218.254';
 
     const[teams, setTeams] = useState([]);
 
