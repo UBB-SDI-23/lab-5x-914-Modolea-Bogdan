6c05fc4a7fae4dc63b1099eb651d2b7d19eaffcb
914-modolea-bogdan <bogdan.modolea@stud.ubbcluj.ro>
2023-06-06 22:47:19 +0300

    Scripts

---
+++ required.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install -y openjdk-19-jdk-headless
+sudo apt install maven
+cd /home/ubuntu
+git clone https://github.com/UBB-SDI-23/lab-5x-914-Modolea-Bogdan.git
+sudo apt install nginx
+sudo ufw allow 'Nginx Full'
+sudo apt install git
+sudo nginx -t
+sudo nginx -s reload
+sudo apt install snapd
+sudo snap install --classic certbot
---
+++ run.sh
@@ -0,0 +1,3 @@
+cd lab-5x-914-Modolea-Bogdan/A4
+mvn clean install
+java -jar target/spring-boot-aws.jar
